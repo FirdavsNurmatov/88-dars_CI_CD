0955b7e09b2d75d81d817199f111ddbeef8824fc
FirdavsNurmatov <nurmatovfirdavs96@gmail.com>
2025-01-21 15:13:23 +0500

    updated

--- src/order_product/order_product.module.ts
+++ src/order_product/order_product.module.ts
@@ -3,10 +3,9 @@ import { OrderProductService } from './order_product.service';
 import { OrderProductController } from './order_product.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { OrderProduct } from './entities/order_product.entity';
-import { Product } from '../product/entities/product.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([OrderProduct, Product])],
+  imports: [TypeOrmModule.forFeature([OrderProduct])],
   controllers: [OrderProductController],
   providers: [OrderProductService],
 })
